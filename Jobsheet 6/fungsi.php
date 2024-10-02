5242d8cd780dd2c0b2c5df3eac0b2faff62069a1
restuprtma <restu.prtma05@gmail.com>
2024-10-03 00:47:04 +0700

    fungsi

---
+++ Jobsheet 6/fungsi.php
@@ -0,0 +1,15 @@
+<?php
+function hitungUmur($thn_lahir, $thn_sekarang){
+    $umur = $thn_sekarang - $thn_lahir;
+    return $umur;
+}
+function perkenalan($nama, $salam="Assalamuallaikum"){
+    echo $salam.", ";
+    echo "Perkenalkan, nama saya ".$nama."<br/>";
+
+    echo "Saya berusia ". hitungUmur(2005, 2024) . " tahun<br/>";
+    echo "Senang berkenalan dengan anda<br/>";
+}
+
+perkenalan("Restu");
+?>
