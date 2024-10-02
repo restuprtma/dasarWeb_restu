1618261784ae74789eee48adee9ec1ecbe743feb
restuprtma <restu.prtma05@gmail.com>
2024-10-03 00:45:03 +0700

    php array 2

---
+++ Jobsheet 6/array_2.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        table {
+            width: 50%;
+            border-collapse: collapse;
+            margin: 20px 0;
+            font-family: Arial, sans-serif;
+        }
+        table, th, td {
+            border: 1px solid #dddddd;
+        }
+        th, td {
+            padding: 8px 12px;
+            text-align: left;
+        }
+        th {
+            background-color: #f2f2f2;
+        }
+        tr:nth-child(even) {
+            background-color: #f9f9f9;
+        }
+    </style>
+</head>
+<body>
+    <?php
+        $Dosen = [
+            'nama' => 'Elok Nur Hamdana',
+            'domisili' => 'Malang',
+            'jenis kelamin' =>  'Perempuan'      
+        ];
+    ?>
+    <h2>Data Dosen</h2>
+    <table>
+        <tr>
+            <th>Attribute</th>
+            <th>Value</th>
+        </tr>
+        <tr>
+            <td>Nama</td>
+            <td><?php echo $Dosen['nama']; ?></td>
+        </tr>
+        <tr>
+            <td>Domisili</td>
+            <td><?php echo $Dosen['domisili']; ?></td>
+        </tr>
+        <tr>
+            <td>Jenis Kelamin</td>
+            <td><?php echo $Dosen['jenis kelamin']; ?></td>
+        </tr>
+    </table>
+</body>
+</html>
