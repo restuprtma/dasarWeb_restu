0df306e8b181fa77fc2ccb947c1f3106df3ed4c2
restuprtma <restu.prtma05@gmail.com>
2024-10-08 07:56:52 +0700

    function isset dalam php

--- Jobsheet 7/isset.php
+++ Jobsheet 7/isset.php
@@ -1,10 +1,10 @@
 <?php
-// $umur;
-// if (isset($umur) && $umur >= 18) {
-//     echo "Anda sudah dewasa";
-// } else {
-//     echo "Anda belum dewasa atau variabel 'umur' tidak ditemukan";
-// }
+$umur;
+if (isset($umur) && $umur >= 18) {
+    echo "Anda sudah dewasa";
+} else {
+    echo "Anda belum dewasa atau variabel 'umur' tidak ditemukan";
+}
 
 $data = array("nama" => "Jane", "usia" => 25);
 if (isset($data["nama"])) {
