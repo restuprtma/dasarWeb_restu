8fbefe90e5dd193a2aaac3082b684fa4f1a99d2e
restuprtma <restu.prtma05@gmail.com>
2024-10-08 08:06:25 +0700

    function empty dalam php

---
+++ Jobsheet 7/empty.php
@@ -0,0 +1,16 @@
+<?php
+$myArray = array();
+if(empty($myArray)){
+    echo " Array tidak terdefinisi atau kosong";
+}else{
+    echo "Array sudah terdefinisi dan tidak kosong";
+}
+echo "<br>";
+
+if(empty($nonExistentVar)){
+    echo " Variable tidak terdefinisi atau kosong";
+}else{
+    echo "Variable sudah terdefinisi dan tidak kosong";
+}
+
+?>
